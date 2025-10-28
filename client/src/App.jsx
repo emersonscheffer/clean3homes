7b03729c72066a4763e46870e8d9be7d43608572
emersonscheffer <32528159+emersonscheffer@users.noreply.github.com>
2025-10-27 23:56:37 -0700

    add react files

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import cleanLogo from "/clean3homesLogo.jpeg";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={cleanLogo} width="100px" />
+      </div>
+    </>
+  );
+}
+
+export default App;
